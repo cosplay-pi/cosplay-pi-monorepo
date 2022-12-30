38dc51724d5a2f258e9a7f4ede3b28e61c976d9b
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2022-12-30 19:36:42 +0100

    Add prisma to hub-backend

--- packages/web/costume-chip-hub-backend/src/main.ts
+++ packages/web/costume-chip-hub-backend/src/main.ts
@@ -1,4 +1,9 @@
 import * as express from 'express';
+import * as crypto from 'crypto';
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 import {
   DeviceCommandInfo,
@@ -113,4 +118,122 @@ app.get(
   },
 );
 
-app.listen(4000, () => { });
+const getDevicePublicKey = ({
+  devicePrivateKey,
+}: {
+  devicePrivateKey: crypto.KeyObject;
+}) => {
+
+  return crypto.createPublicKey(devicePrivateKey);
+};
+
+const getDeviceId = ({
+  devicePublicKey,
+}: {
+  devicePublicKey: crypto.KeyObject;
+}) => {
+
+  const devicePublicKeyAsDer = devicePublicKey.export({
+    type: `spki`,
+    format: `der`,
+  });
+
+  return crypto.createHash(`md5`).update(devicePublicKeyAsDer).digest(`hex`);
+}
+
+app.get(
+  `/register-user-device`,
+  async (request, response) => {
+
+    const userIdToken = request.query[`userIdToken`] as string;
+    const devicePrivateKeyKty = request.query[`devicePrivateKeyKty`] as string;
+    const devicePrivateKeyN = request.query[`devicePrivateKeyN`] as string;
+    const devicePrivateKeyE = request.query[`devicePrivateKeyE`] as string;
+    const devicePrivateKeyD = request.query[`devicePrivateKeyD`] as string;
+    const devicePrivateKeyP = request.query[`devicePrivateKeyP`] as string;
+    const devicePrivateKeyQ = request.query[`devicePrivateKeyQ`] as string;
+    const devicePrivateKeyDp = request.query[`devicePrivateKeyDp`] as string;
+    const devicePrivateKeyDq = request.query[`devicePrivateKeyDq`] as string;
+    const devicePrivateKeyQi = request.query[`devicePrivateKeyQi`] as string;
+
+    console.log(userIdToken);
+    
+    const userId = `testuser`;
+
+    const devicePrivateKey = crypto.createPrivateKey({
+      key: {
+        kty: devicePrivateKeyKty,
+        n: devicePrivateKeyN,
+        e: devicePrivateKeyE,
+        d: devicePrivateKeyD,
+        p: devicePrivateKeyP,
+        q: devicePrivateKeyQ,
+        dp: devicePrivateKeyDp,
+        dq: devicePrivateKeyDq,
+        qi: devicePrivateKeyQi,
+      },
+      format: `jwk`,
+    });
+
+    const devicePublicKey = getDevicePublicKey({
+      devicePrivateKey,
+    });
+
+    const deviceId = getDeviceId({
+      devicePublicKey,
+    });
+
+    const devicePrivateKeyAsPem = devicePrivateKey.export({
+      type: `pkcs1`,
+      format: `pem`,
+    }) as string;
+    
+    await prisma.device.create({
+      data: {
+        id: deviceId,
+        userId: userId,
+        privateKeyAsPem: devicePrivateKeyAsPem,
+      },
+    });
+
+    response.json({
+      deviceId,
+    });
+  },
+);
+
+app.get(
+  `/fetch-user-devices-info`,
+  async (request, response) => {
+
+    const userIdToken = request.query[`userIdToken`] as string;
+
+    console.log(userIdToken);
+    
+    const userId = `testuser`;
+
+    const userDevicesDbInfo = await prisma.device.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+
+    const userDevicesInfo: {
+      [userDeviceId: string]: {},
+    } = {};
+
+    for (const userDeviceDbInfo of userDevicesDbInfo) { 
+
+      userDevicesInfo[userDeviceDbInfo.id] = {};
+    }
+
+    response.json(
+      userDevicesInfo,
+    );
+  },
+);
+
+app.listen(4000, async () => {
+
+  console.log(`Hello!`);
+});
