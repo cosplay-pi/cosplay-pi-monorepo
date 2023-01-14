f1030a743364cda0cf12674eb7975206514f1d2d
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2023-01-14 11:35:03 +0100

    Disable hub-backend CORS

--- packages/web/cosplay-pi-hub-backend/src/hub-backend-express-app.ts
+++ packages/web/cosplay-pi-hub-backend/src/hub-backend-express-app.ts
@@ -1,4 +1,9 @@
+import * as cors from 'cors';
 import * as express from 'express';
 
 export const hubBackendExpressApp = express();
+
+hubBackendExpressApp.use(cors({
+  origin: `*`,
+}));
 hubBackendExpressApp.use(express.json());
