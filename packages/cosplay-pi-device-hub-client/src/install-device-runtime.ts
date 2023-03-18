111eaaa6ba8427a6d6bdda898e45e8d52074a54c
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2023-03-18 10:15:56 +0100

    Use yarn berry for hub-client runtime

--- packages/cosplay-pi-device-hub-client/src/install-device-runtime.ts
+++ packages/cosplay-pi-device-hub-client/src/install-device-runtime.ts
@@ -1,4 +1,6 @@
 import { spawn } from "child_process";
+import * as fs from "fs";
+import * as path from "path";
 
 import { createDeviceRuntimeDir } from "./create-device-runtime-dir";
 import { fetchDeviceRuntimeProcess } from "./device-runtime-process";
@@ -17,8 +19,16 @@ export const installDeviceRuntime = async () => {
 
   generateDeviceRuntimeInfoFile();
 
+  fs.writeFileSync(
+    path.resolve(
+      deviceRuntimeDirPath,
+      `yarn.lock`,
+    ),
+    ``,
+  );
+
   const yarnInstallDeviceRuntimeProcess = spawn(
-    `yarn`,
+    `yarn set version berry && yarn`,
     {
       shell: true,
       cwd: deviceRuntimeDirPath,
