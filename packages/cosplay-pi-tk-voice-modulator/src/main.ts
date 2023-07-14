3418221be53b334c6acb0593a6f598547d2014cd
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2023-07-15 01:25:31 +0200

    Refactor cosplay-pi-tk-voice-modulator

--- packages/cosplay-pi-tk-voice-modulator/src/main.ts
+++ packages/cosplay-pi-tk-voice-modulator/src/main.ts
@@ -1,11 +1,14 @@
 import {
-  ChildProcess,
+  ChildProcessWithoutNullStreams,
   spawn,
 } from 'child_process';
 
 import { tkVoiceModulatorEffectiveSettings } from './tk-voice-modulator-settings';
 
 const init = async () => {
+
+  console.log(`This module uses these sounds from freesound:`);
+  console.log(`- "radio click 7.wav" by ERH ( https://freesound.org/people/ERH/sounds/30331/ ) licensed under CCBY 4.0`);
 
   const recArgs = [
     `--buffer`,
@@ -29,40 +32,85 @@ const init = async () => {
     tkVoiceModulatorEffectiveSettings.compand2EffectArgs,
   ];
 
-  let soxProcess: ChildProcess | undefined;
+  const startSox = () => {
 
-  const startSoxProcess = () => {
-
-    soxProcess = spawn(
-      `nice -n 8 play "|rec ${recArgs.join(` `)}"`,
+    let soxInputProcess: ChildProcessWithoutNullStreams | undefined = spawn(
+      `nice -n 8 sox -d -t wav - ${recArgs.join(` `)}`,
       {
         shell: true,
-        stdio: `inherit`,
       },
     );
 
-    soxProcess.on(
+    let soxOutputProcess: ChildProcessWithoutNullStreams | undefined = spawn(
+      `play -t wav -`,
+      {
+        shell: true,
+      },
+    );
+
+    soxInputProcess.stdout.pipe(soxOutputProcess.stdin);
+    soxInputProcess.stderr.on(`data`, () => {
+    });
+
+    soxOutputProcess.stdout.pipe(process.stdout);
+    soxOutputProcess.stderr.on(`data`, () => {
+    });
+
+    const onSoxTerminated = async () => {
+
+      await new Promise((resolvePromise) => setTimeout(resolvePromise, 1000));
+
+      if (soxInputProcess !== undefined) {
+
+        try {
+
+          soxInputProcess.kill();
+
+        } catch {
+        } finally {
+
+          soxInputProcess = undefined;
+        }
+      }
+
+      if (soxOutputProcess !== undefined) {
+
+        try {
+
+          soxOutputProcess.kill();
+
+        } catch {
+        } finally {
+
+          soxOutputProcess = undefined;
+        }
+      }
+
+      startSox();
+    };
+
+    soxInputProcess.on(
       `exit`,
-      async () => {
-
-        await new Promise((resolvePromise) => setTimeout(resolvePromise, 1000));
-
-        startSoxProcess();
-      },
+      onSoxTerminated,
     );
 
-    soxProcess.on(
+    soxInputProcess.on(
       `error`,
-      async () => {
+      onSoxTerminated,
+    );
 
-        await new Promise((resolvePromise) => setTimeout(resolvePromise, 1000));
+    soxOutputProcess.on(
+      `exit`,
+      onSoxTerminated,
+    );
 
-        startSoxProcess();
-      },
+    soxOutputProcess.on(
+      `error`,
+      onSoxTerminated,
     );
   };
 
-  startSoxProcess();
+  startSox();
 };
 
 init();
